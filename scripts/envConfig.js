6557e37752e0100bd730c2e4bac66b1b7ed6bdf3
Cristián Ormazábal (Pasamonte) <cristian.ormazabal@pasamonte.cl>
2017-07-11 18:07:41 -0400

    Nuevos archivos

---
+++ scripts/envConfig.js
@@ -0,0 +1,15 @@
+angular.module('sdx.envConfig', [])
+
+.constant('SCRIPTS', '/scripts')
+
+.constant('APP_LANDING', '/Mantenedor/')
+
+.constant('APP_LOGIN', '/Authentication/')
+
+.constant('API_URL', 'http://mantenedordesa.rayensalud.com')
+
+.constant('DEBUG', false)
+
+.constant('TIMEZONE', '-03:00')
+
+;
